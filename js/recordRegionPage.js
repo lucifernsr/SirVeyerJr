1fabe9816788c1680cbbdd1274f12ad0f51d303f
Nuwan S. Rajapakshe <33234642+lucifernsr@users.noreply.github.com>
2018-01-02 21:44:38 +0530

    Update recordRegionPage.js

--- js/recordRegionPage.js
+++ js/recordRegionPage.js
@@ -5,6 +5,6 @@ function initMap() {
     // Initialise map, centred on Melbourne, Australia.        
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 6.9064, lng: 79.9046},
-        zoom: 17        
+        zoom: 19        
     });      
 }
