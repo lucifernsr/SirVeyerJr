073f71222d6a939f0e8e71cb8f44bd3665bc3fc3
Nuwan S. Rajapakshe <33234642+lucifernsr@users.noreply.github.com>
2018-01-02 15:08:03 +0530

    Update recordRegionPage.js to initialize the map.

--- js/recordRegionPage.js
+++ js/recordRegionPage.js
@@ -1,2 +1,10 @@
 // Code for the Record Region page.
 
+// Map Initialisation callback.  Will be called when Maps API loads.
+function initMap() {        
+    // Initialise map, centred on Melbourne, Australia.        
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -37.8200855, lng: 144.9608045},
+        zoom: 17        
+    });      
+}
