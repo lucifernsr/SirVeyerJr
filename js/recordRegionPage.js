7cc283a0c8d1df47c20267e53c1b3f5d0191a9bd
Nuwan S. Rajapakshe <33234642+lucifernsr@users.noreply.github.com>
2018-01-04 00:54:40 +0530

    Update recordRegionPage.js
    Test01 - HTML5 Geolocation, watchPosition() method.

--- js/recordRegionPage.js
+++ js/recordRegionPage.js
@@ -1,4 +1,5 @@
 // Code for the Record Region page.
+var map, infoWindow;
 
 // Map Initialisation callback.  Will be called when Maps API loads.
 function initMap() {        
@@ -6,5 +7,34 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 6.9064, lng: 79.9046},
         zoom: 19        
-    });      
+    });
+    
+    
+    infoWindow = new google.maps.InfoWindow;
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Current Location.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter()); 
+    }  
+}
+      
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ? 
+                          'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
