2a6f02df07fa8cc6d81f1f0d6d5489ac12e81efb
Nuwan S. Rajapakshe <33234642+lucifernsr@users.noreply.github.com>
2018-01-10 22:02:32 +0530

    Update viewRegionPage.js

--- js/viewRegionPage.js
+++ js/viewRegionPage.js
@@ -1,14 +1,60 @@
 // Code for the View Region page.
+var map, infoWindow, locationInaccuracy, currentPos, regionPolygon;
+var calculatedData = {area:undefined,
+                     perimeter:undefined};
 
 // The following is sample code to demonstrate navigation.
 // You need not use it for final app.
 
-var regionIndex = localStorage.getItem(APP_PREFIX + "-selectedRegion"); 
-if (regionIndex !== null)
-{
-    // If a region index was specified, show name in header bar title. This
-    // is just to demonstrate navigation.  You should set the page header bar
-    // title to an appropriate description of the region being displayed.
-    var regionNames = [ "Region A", "Region B" ];
-    document.getElementById("headerBarTitle").textContent = regionNames[regionIndex];
+var regionIndex = localStorage.getItem(APP_PREFIX + "-selectedRegion");
+var regionInstance = JSON.parse(localStorage.getItem(`${APP_PREFIX}.Region${regionIndex}`));
+
+//regionInstance._cornerLocations.push(regionInstance._cornerLocations[0]);
+
+var areaRef = document.getElementById("area");
+var perimeterRef = document.getElementById("perimeter");
+
+function onloadFunctionViewRegion() {
+    if (regionIndex !== null) {
+        // If a region index was specified, show name in header bar title. This
+        // is just to demonstrate navigation.  You should set the page header bar
+        // title to an appropriate description of the region being displayed.
+        document.getElementById("headerBarTitle").textContent = regionInstance._nickname;
+        
+        areaRef.textContent = calculatedData.area;
+        perimeterRef.textContent = calculatedData.perimeter;
+    }
+}
+
+function togglePosts() {
+    
+}
+
+function centerOnRegion() {
+    
+}
+
+function initMap() {
+    // Initialise map, centred on UCL, Sri Lanka.        
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 6.9064, lng: 79.9046},
+        zoom: 16    
+    });
+    
+    // Initialize the region polygon.
+    regionPolygon = new google.maps.Polygon({
+        path: regionInstance._cornerLocations,
+        geodesic: true,
+        strokeColor: '#800080',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#800080',
+        fillOpacity: 0.40
+    });
+    
+    regionPolygon.setMap(map);
+    calculatedData.area = google.maps.geometry.spherical.computeArea({path: regionInstance._cornerLocations});
+    calculatedData.perimeter = google.maps.geometry.spherical.computeLength({path: regionInstance._cornerLocations});
+    console.log(calculatedData.area);
+    console.log(calculatedData.perimeter);
 }
