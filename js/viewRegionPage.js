61795f1fa24a5f2d58295d6aa20d1f1997fd0eaa
Nuwan S. Rajapakshe <33234642+lucifernsr@users.noreply.github.com>
2018-01-15 14:40:55 +0530

    Update viewRegionPage.js

--- js/viewRegionPage.js
+++ js/viewRegionPage.js
@@ -1,21 +1,34 @@
 // Code for the View Region page.
+/*
+ * Sir Veyer Jr. TM v2.0
+ *
+ * MCD4290 - Assignment 02 (2017 T3)
+ * 
+ * Author(s):   Team 02
+ *              Nuwan Sanjeewa, Raidh Ramzee, Randil Silva, Ujitha Hennayake
+ *
+ * This is the final submission file for the Assignment 02, the surveying app
+ * which contains the key functions required to display a interactive map with
+ * releavent to the Region displaying with following functionalities.
+ *
+ * This file has the following functionalities:
+ *              # Display the physical area of a region in an interactive map.
+ *              # Display the area of the region in squre meters.
+ *              # Display the perimeter of the region in meters.
+ *              # Display on/off the suggested fence post locations in the map.
+ *              # Delete a region.
+*/
+// Global variables used throughout the page.
 var map, regionPolygon, bounds, center, regionInstance;
-
 var fencePostsMarkers = [];
 var postsOn = false;
-
 var regionsList = JSON.parse(localStorage.getItem("localRegions"));
-
-// The following is sample code to demonstrate navigation.
-// You need not use it for final app.
-
 var regionIndex = Number(localStorage.getItem(APP_PREFIX + "-selectedRegion"));
 var regionInstancePDO = JSON.parse(localStorage.getItem(`${APP_PREFIX}.Region${regionIndex}`));
-//regionInstance.cornerLocation = regionInstance.cornerLocations[0];
-
 var areaRef = document.getElementById("area");
 var perimeterRef = document.getElementById("perimeter");
 
+// Callback function from the Google API script file.
 function initMap() {
     regionInstance = new Region(regionInstancePDO.name, regionInstancePDO.date, regionInstancePDO.corners);
     bounds = regionInstance.bounds;
@@ -28,7 +41,6 @@ function initMap() {
     
     // Initialize the region polygon.
     regionPolygon = regionPolygon = createPolygon(regionInstance.cornerLocations, '#0000FF');
-    
     regionPolygon.setMap(map);
     
     // Initialize the markers for fence posts.
@@ -47,6 +59,7 @@ function initMap() {
     }
 }
 
+// View region onload function.
 function onloadFunctionViewRegion() {
     if (regionIndex !== null) {
         // If a region index was specified, show name in header bar title. This
@@ -59,6 +72,7 @@ function onloadFunctionViewRegion() {
     }
 }
 
+// A simple function to display the fence posts on and off. 
 function togglePosts() {
     if (postsOn !== true) {
         for (var item in fencePostsMarkers) {
@@ -72,39 +86,47 @@ function togglePosts() {
         }
         postsOn = false;
     }
+    displayMessage(`Number of Fence Posts required: ${fencePostsMarkers.length}`,60000);
 }
 
+// A simple function to center and fit the region to the screen. 
 function centerOnRegion(bounds, center) {
     map.setCenter(center);
     map.fitBounds(bounds);
 }
 
+// A simple function to call the centerOnRegion() with a button click. 
 function centerButton() {
     centerOnRegion(bounds, center);
 }
 
+// A simple function to delete a region from the memory with a button click.
 function deleteRegion() {
+    // Prompting a confirmation box to obtain the approval for removing the last point.
     if (confirm("Are you sure that you want to permanatly delete this region?")) {
+        // The method to clear the region if it's the only region.
         if ((regionIndex === 0) && (regionsList.length === 1)) {
             localStorage.clear();
             location.href = 'index.html';
         }
         else {
+            // Creating the current and the next region keys dynamically to refer in the next code block.
             var regionKey = `${APP_PREFIX}.Region${regionIndex}`;
             localStorage.removeItem(regionKey);
     
+            // Modifing all the remaining elements of the localRegions list.
             for (var j = regionIndex + 1; j <= regionsList.length; j++) {
                 var lastKey = `${APP_PREFIX}.Region${j-1}`
                 var thisKey = `${APP_PREFIX}.Region${j}`;
                 var thisRegionPDO = localStorage.getItem(thisKey);
                 localStorage.setItem(lastKey, thisRegionPDO);
             }
-    
+            
+            // Remove the last 'null' entry from the list and redirecting to the updated index page.
             localStorage.removeItem(`${APP_PREFIX}.Region${regionsList.length}`);
             regionsList.pop();
             localStorage.setItem("localRegions", JSON.stringify(regionsList));
             location.href = 'index.html';
         }
     }
-    
 }
